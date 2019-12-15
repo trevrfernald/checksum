475291425c3d46487db0f76ec0f203ca2a751856
unknown <trevr.fernald@gmail.com>
2019-12-15 09:12:06 -0700

    updating tests

--- test_checksum.py
+++ test_checksum.py
@@ -1,16 +1,20 @@
-import unittest
-from unittest.mock import patch
-import checksum
+from checksum_gui import DataSet
+import pytest
 
 # setUp / tearDown for files?
 # https://youtu.be/6tNS--WetLI?t=1850
 # ^ Python Tutorial: Unit Testing Your Code with the unittest Module
 
-def test_choose_hash(self):
-    self.assertEqual(choose_hash("sha256", file, checksum), )
+# arrange, act, assert
 
-def test_scan(self):
-    pass # use mocking for testing data from a site
-    with patch(requests.get) as mocked_get:
-        mocked_get.return_value.ok = True
-        mocked_get.return_value.text = True
+
+@pytest.fixture
+def instance():
+    checksum = "99D51AD7B1CC518082E7E73A56DE24DE249CD0D5090C78DAE87A591F96E081BA"
+    return DataSet(1, "C:/Users/T/Downloads/cmder.7z", checksum)
+
+
+def test_calculate_checksum(instance):
+    sha256_hash = "99D51AD7B1CC518082E7E73A56DE24DE249CD0D5090C78DAE87A591F96E081BA".lower()
+    assert DataSet.calculate_checksum(instance) == sha256_hash
+    # assert DataSet.compare(instance) is True
